704c00091bd4dd0c56a91af3dfabde97a352f985
Israel Wong <israelwong@icloud.com>
2025-09-14 11:28:37 -0600

    fix error

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,7 +5,7 @@ function getStripeInstance() {
     if (!process.env.STRIPE_SECRET_KEY) {
         throw new Error("STRIPE_SECRET_KEY is not set");
     }
-    
+
     return new Stripe(process.env.STRIPE_SECRET_KEY!, {
         apiVersion: "2023-10-16",
         typescript: true,
